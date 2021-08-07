90da17eb0cc8aad3c4551bb03b6f35fef0d57ccc
adirajput2000 <adirajput2000@gmail.com>
2021-08-07 09:37:32 +0530

    check

--- findmax.cpp
+++ findmax.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main(){ 
 int a , b , c;
 cout <<"Please enter the first numbe -";
 cin >> a;
---
+++ oddeven.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+int a ;
+cout << "Please enter a number - ";
+cin >> a;
+
+if(a%2 == 0){
+
+    cout <<"The entered number is EVEN";
+
+}
+
+else{
+
+    cout << "The entered number is ODD";
+}
+
+
+}
