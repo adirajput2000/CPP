2b851926a30ae05416c151ebec137e23b309acd3
adirajput2000 <adirajput2000@gmail.com>
2021-08-07 09:30:44 +0530

    first

---
+++ findmax.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a , b , c;
+cout <<"Please enter the first numbe -";
+cin >> a;
+
+cout <<"Please enter the second number -";
+cin >> b;
+
+cout <<"Please enter the third number - ";
+cin >> c;
+
+if(a>b){
+    if (a>c)
+    {
+        cout << "The Maximum No. is - ";
+        cout << a;
+
+
+    }
+    
+    else {
+
+
+        
+        cout << "The Maximum No. is";
+        cout << c;
+
+    }
+    
+}
+
+else{
+
+    if(b>c){
+
+        cout << "The Maximum No. is";
+        cout << b;
+    }
+
+    else{
+        cout << "The Maximum No. is";
+        cout << c;
+    }
+
+}
+
+
+
+
+
+
+}
